ef1d4da7a7a8290d5c7a81b062c3f9e8fd123016
haoweiwang0 <15011424380@163.com>
2024-07-23 23:19:29 +0800

    modified:   models.py

--- models.py
+++ models.py
@@ -150,3 +150,31 @@ class ResNet(nn.Module):
 
 def ResNet18(input_dim):
     return ResNet(input_dim, BasicBlock, [2, 2, 2, 2])
+
+class ConvNet_cifar(nn.Module):
+    def __init__(self, output_dim):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5, 1)
+        self.dp1 = torch.nn.Dropout(0.5)
+        self.conv2 = nn.Conv2d(6, 16, 5, 1)
+        self.dp2 = torch.nn.Dropout(0.5)
+        self.fc1 = nn.Linear(4 * 4 * 16, 120)
+        self.dp3 = torch.nn.Dropout(0.5)
+        self.fc2 = nn.Linear(120, 84)
+        self.dp4 = torch.nn.Dropout(0.5)
+        self.fc3 = nn.Linear(84, output_dim)
+
+    def forward(self, x):
+        x = self.embed(x)
+        x = self.fc3(x)
+        return x
+
+    def embed(self, x):
+        x = F.relu(self.dp1(self.conv1(x)))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.dp2(self.conv2(x)))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4 * 4 * 16)
+        x = F.relu(self.dp3(self.fc1(x)))
+        x = F.relu(self.dp4(self.fc2(x)))
+        return x
