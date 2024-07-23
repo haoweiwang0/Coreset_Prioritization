12d1ad9959ee3b145245b2e7647ffa57c855f905
haoweiwang0 <15011424380@163.com>
2024-07-24 00:51:18 +0800

    modified:   models.py

--- models.py
+++ models.py
@@ -158,7 +158,7 @@ class ConvNet_cifar(nn.Module):
         self.dp1 = torch.nn.Dropout(0.5)
         self.conv2 = nn.Conv2d(6, 16, 5, 1)
         self.dp2 = torch.nn.Dropout(0.5)
-        self.fc1 = nn.Linear(8 * 8 * 16, 120)
+        self.fc1 = nn.Linear(5 * 5 * 16, 120)
         self.dp3 = torch.nn.Dropout(0.5)
         self.fc2 = nn.Linear(120, 84)
         self.dp4 = torch.nn.Dropout(0.5)
@@ -174,7 +174,7 @@ class ConvNet_cifar(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.dp2(self.conv2(x)))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 8 * 8 * 16)
+        x = x.view(-1, 5 * 5 * 16)
         x = F.relu(self.dp3(self.fc1(x)))
         x = F.relu(self.dp4(self.fc2(x)))
         return x
